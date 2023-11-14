650d129cc7252dbb401412a2ab78c84be178dfd3
컴퓨터\kim-jin-young <wlsdud11457@gmail.com>
2023-11-14 23:30:56 +0900

    기능 추가 : 회원가입 기능 구현

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
-
 const express = require('express');
 const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
+const mysql = require('mysql2');
 
 // JSON body parser 설정
 app.use(bodyParser.json());
@@ -12,8 +12,24 @@ app.use(cors());
 
 app.set('port', process.env.PORT || 3000);
 
+// MySQL 연결 설정
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'rlawlsdud11!',
+    database: 'mapleinfodb'
+});
+
+// MySQL 연결
+connection.connect((err) => {
+    if (err) {
+        console.error('MySQL 연결 에러:', err);
+        throw err;
+    }
+    console.log('MySQL에 성공적으로 연결되었습니다.');
+});
+
 app.post('/signUp', (req, res) => {
-    // 클라이언트로부터의 POST 요청 데이터 추출
     const { id, passWord, nickName, phoneNumber } = req.body;
 
     console.log('ID:', id);
@@ -21,8 +37,21 @@ app.post('/signUp', (req, res) => {
     console.log('Nickname:', nickName);
     console.log('Phone Number:', phoneNumber);
 
-    // 클라이언트에 응답 전송
-    res.json({ message: '데이터가 성공적으로 전송되었습니다.' });
+    // SQL 쿼리 정의
+    const sql = `INSERT INTO user (userId, userPw, userNickName, userPhoneNum, userRegDate) 
+    VALUES (?, ?, ?, ?, NOW())`;
+
+    // SQL 쿼리 실행
+    connection.query(sql, [id, passWord, nickName, phoneNumber], (err, result) => {
+        if (err) {
+            console.error('INSERT 에러:', err);
+            res.status(500).json({ message: '회원가입 실패.' });
+            return;
+        }
+
+        console.log('회원가입 성공.', result);
+        res.json({ message: '데이터가 성공적으로 전송되었습니다.' });
+    });
 });
 
 app.listen(app.get('port'), () => {
