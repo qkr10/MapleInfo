acd98d82f1fea12cc36820ecc04c0ba438023aac
컴퓨터\kim-jin-young <wlsdud11457@gmail.com>
2023-11-27 19:22:10 +0900

    Refactor - MainContents 수정

--- client/src/Component/MainContents.jsx
+++ client/src/Component/MainContents.jsx
@@ -1,20 +1,44 @@
 import React from "react";
 import '../App.css';
+import '../css/MainContents.css'
+import testImg from '../img/testImg.png'
+import Button from 'react-bootstrap/Button';
 
 function MainContents(){
     return(
         <div>
-            <div>
-                <p>이번 주 무릉도장 1등</p>
-            </div>
-            <div>
-                <p>이번 주 더시드 1등</p>
-            </div>
-            <div>
-                <p>이번 주 업적 1등</p>
-            </div>
-            <div>
-                <p>이번 주 유니온 1등</p>
+            <div className={"testContainer"}>
+                <div className={"weakly-view"}>
+                    <div className={"weakly-view-header"}>
+                        <span>이번 주 무릉도장 1등</span>
+                    </div>
+                    <div>
+                        <p>밥먹어친구야 LV281 듀얼블레이더</p>
+                    </div>
+                    <div>
+                        <img src={testImg} alt="테스트 이미지" />
+                    </div>
+                    <div>
+                        <p>Lv.10724</p>
+                    </div>
+                    <div>
+                        전투력 1,068,221,425
+                    </div>
+                    <div>
+                        <Button variant="dark" className="view-details-btn">
+                            <a href="#" className="view-details">상세보기</a>
+                        </Button>
+                    </div>
+                </div>
+                <div>
+                    <p>이번 주 더시드 1등</p>
+                </div>
+                <div>
+                    <p>이번 주 업적 1등</p>
+                </div>
+                <div>
+                    <p>이번 주 유니온 1등</p>
+                </div>
             </div>
             <div>
                 <p> 금일 일반월드 랭킹</p>
